10e6051213505ea6b25777c3a3eef00f17bc046e
Alex Parloti <parloti@gmail.com>
2017-05-31 10:31:02 +0200

    Quotes requisition

--- script.js
+++ script.js
@@ -1,100 +1,59 @@
 'use strict';
 angular
-	.module('randomQuoteMachine')
-	.controller('dynamicColors', ['$scope', function ($scope) {
-		$scope.headerColor = "#FFFFFF";
-		$scope.divStyle = {
-			"background-color": $scope.headerColor
-		}
-	}])
-	.controller('dynamicQuote', ['$scope', function ($scope) {
-		$scope.quotes = [
-			{
-				name: 'About',
-				iconClass: 'fa fa-info-circle icon-white',
-				isVisible: true,
-				isActive: true
-			},
-			{
-				name: 'Skills',
-				iconClass: 'fa fa-wrench icon-white',
-				isVisible: false,
-				isActive: true
-			},
-			{
-				name: 'Projects',
-				iconClass: 'fa fa-cogs icon-white',
-				isVisible: true,
-				isActive: true
-			},
-			{
-				name: 'Testimonials',
-				iconClass: 'fa fa-comments icon-white',
-				isVisible: false,
-				isActive: true
-			},
-			{
-				name: 'Articles',
-				iconClass: 'fa fa-pencil-square-o icon-white',
-				isVisible: false,
-				isActive: true
-			},
-			{
-				name: 'Contacts',
-				iconClass: 'fa fa-address-book-o icon-white',
-				isVisible: true,
-				isActive: true,
-				navigation: [
-					{
-						name: 'QR Code',
-						href: '#qr-code-panel',
-						iconClass: 'fa fa-user-circle-o',
-						isVisible: true,
-						isActive: true
-					},
-					{
-						name: 'vCard',
-						href: '#vcard-panel',
-						iconClass: 'fa fa-address-card-o',
-						isVisible: true,
-						isActive: true
-					},
-					{
-						name: 'Social',
-						href: '#social-panel',
-						iconClass: 'fa fa-share-alt',
-						isVisible: true,
-						isActive: true
-					},
-					{
-						name: 'Message',
-						href: '#message-panel',
-						iconClass: 'fa fa-envelope-o',
-						isVisible: true,
-						isActive: true
-					}
-				],
-				content: [
-					{
-						caption: 'Add Me to Your Contacts List'
-					},
-					{
-						caption: 'Add Me to Your Contacts List'
-					},
-					{
-						caption: 'Add Me to Your Contacts List'
-					},
-					{
-						caption: 'Add Me to Your Contacts List'
-					},
-				]
-			},
-			{
-				name: 'Footer',
-				iconClass: 'fa fa-arrow-down',
-				isVisible: true,
-				isActive: true
+	.module('randomQuoteMachine', [])
+	.controller('dynamicQuote', ['$scope', '$http', function ($scope, $http) {
+		var colors = ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGrey', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DimGrey', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'Gold', 'GoldenRod', 'Gray', 'Grey', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey', 'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'RebeccaPurple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen']
+		var req = {
+			type: "GET",
+			url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=",
+			dataType: "json",
+			headers: {
+				"X-Mashape-Key": "xqSCrxrwg9msh4iUnJDhRR99rbdqp11C33OjsnstdvmVwwTgpp",
+				"Accept": "application/json"
 			}
-		];
+		};
 		
+		function getRandomInt(min, max) {
+			min = Math.ceil(min);
+			max = Math.floor(max);
+			return Math.floor(Math.random() * (max - min)) + min;
+		}
+		
+		function newColor() {
+			var colorIndex = getRandomInt(0, colors.length);
+			return colors[colorIndex];
+		}
+		
+		function makeTwitterLink() {
+			var baselink = 'https://twitter.com/intent/tweet?hashtags=quotes,parloti&related=freecodecamp&text='
+			return baselink + '"' + $scope.quote.message + '" ' + $scope.quote.author;
+		}
+		
+		function makeTumblrLink() {
+			var prefixLink = 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=';
+			var suffix = '&canonicalUrl=https://www.tumblr.com/buttons&shareSource=tumblr_share_button';
+			return prefixLink + $scope.quote.author + '&content=' + $scope.quote.message + suffix;
+		}
+		
+		$scope.quote = {
+			message: "",
+			author: "",
+			color: ""
+		};
+		$scope.share = {
+			twitter: "",
+			tumblr: ""
+		}
+		$scope.newQuote = function () {
+			$http(req).then(function (response) {
+				$scope.quote.message = response.data.quote;
+				$scope.quote.author = response.data.author;
+				$scope.quote.color = newColor();
+				$scope.share.twitter = makeTwitterLink();
+				$scope.share.tumblr = makeTumblrLink();
+			}, function (a, b, c, d) {
+				console.log(a, b, c, d);
+			});
+		}
+		$scope.newQuote();
 	}]);
